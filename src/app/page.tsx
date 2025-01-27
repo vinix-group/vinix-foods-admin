f8932f5576e04c67b8e51c7d7ec9de51f1590fc6
Minh Mai <minhmmai@gmail.com>
2025-01-27 14:52:49 +1100

    add test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,12 +23,16 @@ const Home = () => {
               Sign up
             </button>
           </div>
-          {user && <div>
-            <p>You are logged in.</p>
-            <button className="py-3 px-5 sm:ms-4 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={() => router.push("/api/auth/logout")}>
-              Sign out
-            </button>
-          </div>}
+          {user
+            ? <div>
+              <p>You are logged in.</p>
+              <button className="py-3 px-5 sm:ms-4 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={() => router.push("/api/auth/logout")}>
+                Sign out
+              </button>
+            </div>
+            : <div>
+              <p>You are NOT logged in.</p>
+            </div>}
         </div>
       </section>
     </div>
