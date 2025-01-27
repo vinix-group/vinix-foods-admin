674f778c21aef755fc91e91b7d96e7659df5c060
Minh Mai <minhmmai@gmail.com>
2025-01-27 13:56:10 +1100

    fix login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client"
-import { useUser } from '@auth0/nextjs-auth0/client';
 import { useRouter } from 'next/navigation'
 
 const Home = () => {
   const router = useRouter()
-  const { user = null } = useUser();
 
   return (
     <div className="flex-col content-center h-screen">
@@ -12,7 +10,7 @@ const Home = () => {
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
           <h2 className="mb-4 text-3xl font-bold tracking-tight leading-none text-gray-900 dark:text-white">Welcome!</h2>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Please log in to start using Vinix Foods admin portal<br />or sign up for an account.</p><p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">If you have any question, feel free to contact us at<br />0423 66 22 55 or info@vinix.com.au</p>
-          {user ? <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <button className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" onClick={() => router.push("/api/auth/login")}>
               Log in
               <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -22,14 +20,7 @@ const Home = () => {
             <button className="py-3 px-5 sm:ms-4 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={() => router.push("/signup")}>
               Sign up
             </button>
-          </div> : <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <button className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" onClick={() => router.push("/api/auth/logout")}>
-              Sign out
-              <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </button>
-          </div>}
+          </div>
         </div>
       </section>
     </div>
