b6a8f918d8216bddf23e6b0ce9a23505f7fa2dff
Minh Mai <minhmmai@gmail.com>
2025-01-27 12:49:19 +1100

    test deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Please log in to start using Vinix Foods admin portal<br />or sign up for an account.</p><p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">If you have any question, feel free to contact us at<br />0423 66 22 55 or info@vinix.com.au</p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <button className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" onClick={() => router.push("/api/auth/login")}>
-              Log in
+              Sign in
               <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
               </svg>
